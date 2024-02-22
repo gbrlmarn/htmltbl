df65e08147409fbdc9c4f29ca60d795821077ce8
Marin Gabriel <marin.gabriel@protonmail.com>
2024-02-22 13:51:02 +0200

    Table rendering...in progress

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module htmltbl
 
 go 1.21.6
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/olekukonko/tablewriter v0.0.5
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	golang.org/x/net v0.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,29 +5,70 @@ import (
 	"io"
 	"net/http"
 	"os"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/olekukonko/tablewriter"
 )
 
+type Table struct {
+	th   []string
+	td   []string
+	cols int
+}
+
 func main() {
-    if len(os.Args) == 1 {
-        fmt.Println("Please specify the link for table extraction")
-        return
-    }
-    if len(os.Args) > 2 {
-        fmt.Println("Only one link is supported")
-        return
-    }
+	if len(os.Args) == 1 {
+		fmt.Println("Please specify the link for table extraction")
+		return
+	}
+	if len(os.Args) > 2 {
+		fmt.Println("Only one link is supported")
+		return
+	}
 
-    resp, err := http.Get(os.Args[1])
-    if err != nil {
-        fmt.Fprint(os.Stderr, err)
-        os.Exit(1)
-    }
-    defer resp.Body.Close()
+	resp, err := http.Get(os.Args[1])
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
 
-    body, err := io.ReadAll(resp.Body)
-    if err != nil {
-        fmt.Fprint(os.Stderr, err)
-        os.Exit(1)
-    }
-    fmt.Println(string(body))
+	tbls, err := Extract(resp.Body)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+    Render(tbls)
+}
+
+func Extract(body io.ReadCloser) ([]Table, error) {
+	doc, err := goquery.NewDocumentFromReader(body)
+	if err != nil {
+		return nil, err
+	}
+	var tables []Table
+	doc.Find("table").Each(func(i int, s *goquery.Selection) {
+		var t Table
+		// find table header
+		s.Find("th").Each(func(i int, s *goquery.Selection) {
+			t.th = append(t.th, s.Text())
+            t.cols += 1
+		})
+		// find table data
+		s.Find("td").Each(func(i int, s *goquery.Selection) {
+            t.td = append(t.td, s.Text())
+		})
+		tables = append(tables, t)
+	})
+	return tables, nil
+}
+
+func Render(tbls []Table) {
+	table := tablewriter.NewWriter(os.Stdout)
+	// Set headers
+	for _, t := range tbls {
+		table.SetHeader(t.th)
+        table.Append(t.td)
+        table.Render()
+	}
 }
