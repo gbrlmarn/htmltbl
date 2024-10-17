0dab07c799aa36d9a42aefb074d88cf5c11c7a29
Gabriel Marin <marin.gabriel@protonmail.com>
2024-10-17 19:32:09 +0300

    Added check for table node

--- extract.go
+++ extract.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 
 	"github.com/PuerkitoBio/goquery"
@@ -21,22 +22,27 @@ func extract(body io.ReadCloser) ([]Table, error) {
 	}
 	var tbls []Table
 	var row []string
-	doc.Find("table").Each(func(i int, tablehtml *goquery.Selection) {
-		var tbl Table
-		// find table row
-		tablehtml.Find("tr").Each(func(i int, rowhtml *goquery.Selection) {
-			// find table header
-			rowhtml.Find("th").Each(func(i int, tableheading *goquery.Selection) {
-				tbl.headings = append(tbl.headings, tableheading.Text())
+	if doc.HasClass("table") {
+		doc.Find("table").Each(func(i int, tablehtml *goquery.Selection) {
+			var tbl Table
+
+			// find table row
+			tablehtml.Find("tr").Each(func(i int, rowhtml *goquery.Selection) {
+				// find table header
+				rowhtml.Find("th").Each(func(i int, tableheading *goquery.Selection) {
+					tbl.headings = append(tbl.headings, tableheading.Text())
+				})
+				// find table data
+				rowhtml.Find("td").Each(func(i int, tablecell *goquery.Selection) {
+					row = append(row, tablecell.Text())
+				})
+				tbl.rows = append(tbl.rows, row)
+				row = nil
 			})
-			// find table data
-			rowhtml.Find("td").Each(func(i int, tablecell *goquery.Selection) {
-				row = append(row, tablecell.Text())
-			})
-			tbl.rows = append(tbl.rows, row)
-			row = nil
+			tbls = append(tbls, tbl)
 		})
-		tbls = append(tbls, tbl)
-	})
+	} else {
+		return nil, fmt.Errorf("Didn't find any table node.\n")
+	}
 	return tbls, nil
 }
